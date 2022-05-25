cde65d14106b0db3f31ec29de1919a0038134a82
Hong <xgketxgket@gmail.com>
2022-05-25 20:31:53 +0800

    加入網址

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const isMobile = $(window).width() <= 480;
+const $ul = $("#SlideLs")
+const $Nav = $("Nav")
 /*測試js按鈕用*/
 // $("#Btb1").click(function () {
 //     alert("Btn1")
@@ -10,41 +12,79 @@ const isMobile = $(window).width() <= 480;
 
 
 $("#Btn1").addClass("js-nav-btn")
-if (isMobile) {
-    console.log("手機板")
-    $('#SlideLs .slide-item').scroll(function () {
-        // console.log($(this).scrollTop());
-        if ($(this).scrollTop() == 0) {
-          $('#Nav').removeClass('js-nav');
-        } else {
-          $('#Nav').addClass('js-nav');
-        }
-      });
+// if (isMobile) {
+//     console.log("手機板")
+//     $U1.scroll(function () {
+//         // console.log($(this).scrollTop());
+//         if ($(this).scrollTop() == 0) {
+//           $Nav.removeClass('js-nav');
+//         } else {
+//           $Nav.addClass('js-nav');
+//         }
+//       });
+//     } else {
+//       console.log('電腦版');
+//     }
+if (isMobile == true) {
+  console.log('手機版才會做的事');
+  $ul.find('.slide-item').scroll(function () {
+    // console.log($(this).scrollTop());
+    if ($(this).scrollTop() == 0) {
+      $Nav.removeClass('js-nav');
     } else {
-      console.log('電腦版');
+      $Nav.addClass('js-nav');
     }
+  });
+}
+
 
 /*event------------------ */
 $('#Btn1').click(function () {
-    $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
+  $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
+  $ul.css({
+    left: 0,
+    top: 0
+  })
 });
 
 $('#Btn2').click(function () {
-    $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
+  $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
+  $ul.css({
+    left: "-100%",
+    top: 0
+  })
 });
 
 $('#Btn3').click(function () {
-    $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
+  $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
+  $ul.css({
+    left: "-200%",
+    top: 0
+  })
 });
 
 $('#Btn4').click(function () {
-    $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
+  $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
+  $ul.css({
+    left: 0,
+    top: "-100%"
+  })
 });
 
 $('#Btn5').click(function () {
-    $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
+  $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
+  $ul.css({
+    left: "-100%",
+    top: "-100%"
+  })
 });
 
 $('#Btn6').click(function () {
-    $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
+  $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
+  $ul.css({
+    left: "-200%",
+    top: "-100%"
+  })
 });
+
+$(".pd-item > a").fancybox();/**/
